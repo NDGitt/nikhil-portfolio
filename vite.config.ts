99b5e841b9f72f5be297ce1cbcd0d3b8e6a756b2
Nik <214939971+NDGitt@users.noreply.github.com>
2025-06-06 16:16:31 -0700

    Fix base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/niikhil-portfolio/' // Change this to match your GitHub repository name
+  base: '/nikhil-portfolio/' // Change this to match your GitHub repository name
 }) 
